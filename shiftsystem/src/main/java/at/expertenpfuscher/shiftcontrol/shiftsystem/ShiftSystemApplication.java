dcbbb808dfbfb154c81ad2fdf1c0818672faf730
Patrick Zdarsky <patrick.zdarsky@htl.moedling.at>
2025-11-11 20:57:59 +0100

    Add ShiftSystem Application

---
+++ shiftsystem/src/main/java/at/expertenpfuscher/shiftcontrol/shiftsystem/ShiftSystemApplication.java
@@ -0,0 +1,11 @@
+package at.expertenpfuscher.shiftcontrol.shiftsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftSystemApplication.class, args);
+    }
+}
