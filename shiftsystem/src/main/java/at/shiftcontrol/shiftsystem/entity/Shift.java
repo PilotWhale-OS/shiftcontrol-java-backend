6c9ee274903299b12cdf2b0d53862b28737b8ca1
Patrick Zdarsky <patrick.zdarsky@htl.moedling.at>
2025-11-11 22:24:02 +0100

    Add initial classes to set structure and add useful utils and custom Exceptions

--- shiftsystem/src/main/java/at/shiftcontrol/shiftsystem/entity/Shift.java
+++ shiftsystem/src/main/java/at/shiftcontrol/shiftsystem/entity/Shift.java
@@ -1,4 +1,20 @@
 package at.shiftcontrol.shiftsystem.entity;
 
+import at.shiftcontrol.shiftsystem.type.LockStatus;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Builder
+@EqualsAndHashCode
 public class Shift {
+    private Long id;
+
+    private String name;
+    private String description;
+
+    // startDate, endDate
+
+    private LockStatus lockStatus;
 }
---
+++ shiftsystem/src/main/java/at/shiftcontrol/shiftsystem/type/LockStatus.java
@@ -0,0 +1,6 @@
+package at.shiftcontrol.shiftsystem.type;
+
+public enum LockStatus {
+    SELF_ASSIGNABLE,
+    LOCKED;
+}
