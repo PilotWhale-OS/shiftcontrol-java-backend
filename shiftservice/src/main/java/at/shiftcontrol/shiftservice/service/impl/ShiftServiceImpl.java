945ea7c7ffef556cf3116e43046b369a8e19e20e
Flo Fuerst <flo.fuerst@outlook.de>
2025-11-29 16:54:29 +0100

    #76 Setup database service and update Docker configuration

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/service/impl/ShiftServiceImpl.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/service/impl/ShiftServiceImpl.java
@@ -2,11 +2,12 @@ package at.shiftcontrol.shiftservice.service.impl;
 
 import at.shiftcontrol.shiftservice.dao.ShiftDao;
 import at.shiftcontrol.shiftservice.service.ShiftService;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ShiftServiceImpl implements ShiftService {
-    private final ShiftDao shiftDao;
+//    private final ShiftDao shiftDao;
 }
