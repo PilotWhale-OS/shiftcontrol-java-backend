d7cc3b31179ffe3c02262ae974ae647ed352d122
Flo Fuerst <flo.fuerst@outlook.de>
2025-12-01 16:29:56 +0100

    #76 Add plan-join endpoints

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/endpoint/ShiftPlanEndpoint.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/endpoint/ShiftPlanEndpoint.java
@@ -1,13 +1,17 @@
 package at.shiftcontrol.shiftservice.endpoint;
 
 import at.shiftcontrol.shiftservice.dto.DashboardOverviewDto;
+import at.shiftcontrol.shiftservice.dto.ShiftPlanJoinRequestDto;
 import at.shiftcontrol.shiftservice.dto.ShiftPlanScheduleDto;
 import at.shiftcontrol.shiftservice.dto.ShiftPlanScheduleSearchDto;
+import at.shiftcontrol.shiftservice.entity.ShiftPlan;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +37,26 @@ public class ShiftPlanEndpoint {
         description = "Get (volunteer related) schedule data for a specific shift plan of an event"
     )
     public ShiftPlanScheduleDto getShiftPlanSchedule(@PathVariable String shiftPlanId, ShiftPlanScheduleSearchDto shiftPlanScheduleSearchDto) {
+        return null; // TODO: implement
+    }
+
+    @GetMapping("/details")
+    // TODO Security
+    @Operation(
+        operationId = "getShiftPlanDetails",
+        description = "Get detailed information about a specific shift plan of an event"
+    )
+    public ShiftPlan getShiftPlanDetails(@PathVariable String shiftPlanId) {
+        return null; // TODO: implement
+    }
+
+    @PostMapping("/join")
+    // TODO Security
+    @Operation(
+        operationId = "joinShiftPlan",
+        description = "Join a shift plan using an invite code"
+    )
+    public ShiftPlan joinShiftPlan(@PathVariable String shiftPlanId, @RequestBody ShiftPlanJoinRequestDto requestDto) {
         return null; // TODO: implement
     }
 }
