d55ed2f21eb46310d05d272238753a49ef1adbbd
Paul Spoerker <paul.spoerker@tuwien.ac.at>
2025-12-03 17:27:14 +0100

    #80 add assignments to PositionSlot

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/entity/PositionSlot.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/entity/PositionSlot.java
@@ -25,10 +25,8 @@ public class PositionSlot {
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
 
-    // private Collection<Volunteer> assignedVolunteers; // TODO paul needed ? Assignment exists
-
-    //@NonNull
-    //private Location location; // TODO flo patrick needed or stays in shift?
+    @OneToMany(mappedBy = "positionSlot")
+    private Collection<Assignment> assignments;
 
     @Column(nullable = false)
     private int desiredVolunteerCount;
