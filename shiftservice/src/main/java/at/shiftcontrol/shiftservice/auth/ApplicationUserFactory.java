498de736f14f87756a11204ba55aa95ef1e9c891
Patrick Zdarsky <e12123697@student.tuwien.ac.at>
2025-12-20 17:45:40 +0100

    #10 Remove unused role filtering method from ApplicationUserFactory

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/auth/ApplicationUserFactory.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/auth/ApplicationUserFactory.java
@@ -1,9 +1,6 @@
 package at.shiftcontrol.shiftservice.auth;
 
 import java.util.Collections;
-import java.util.List;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import at.shiftcontrol.shiftservice.auth.user.AdminUser;
 import at.shiftcontrol.shiftservice.auth.user.AssignedUser;
@@ -15,11 +12,5 @@ public class ApplicationUserFactory {
             case ASSIGNED -> new AssignedUser(Collections.emptyList(), username, userId, attributeProvider);
             case ADMIN -> new AdminUser(Collections.emptyList(), username, userId);
         };
-    }
-
-    public List<SimpleGrantedAuthority> filterAndTransformRoles(List<String> roles) {
-        return roles == null ? Collections.emptyList() : roles.stream()
-                .filter(role -> !role.startsWith("ui_"))
-                .map(SimpleGrantedAuthority::new).toList();
     }
 }
