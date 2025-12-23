b9b17a1750ff069fa176cbc4c10a0012bf5c1d32
Patrick Zdarsky <e12123697@student.tuwien.ac.at>
2025-12-23 12:45:36 +0100

    #124 Remove unused ShiftDto conversion methods and clean up ShiftPlanServiceImpl

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/endpoint/EventEndpoint.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/endpoint/EventEndpoint.java
@@ -1,6 +1,20 @@
 package at.shiftcontrol.shiftservice.endpoint;
 
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import at.shiftcontrol.lib.exception.NotFoundException;
 import at.shiftcontrol.lib.util.ConvertUtil;
@@ -8,15 +22,9 @@ import at.shiftcontrol.shiftservice.auth.ApplicationUserProvider;
 import at.shiftcontrol.shiftservice.dto.EventDto;
 import at.shiftcontrol.shiftservice.dto.EventShiftPlansOverviewDto;
 import at.shiftcontrol.shiftservice.dto.ShiftPlanDto;
+import at.shiftcontrol.shiftservice.dto.UnavailabilityCreateDto;
+import at.shiftcontrol.shiftservice.dto.UnavailabilityDto;
 import at.shiftcontrol.shiftservice.service.EventService;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -35,7 +43,6 @@ public class EventEndpoint {
     public List<EventDto> getAllEvents() throws NotFoundException {
         return eventService.search(null, userProvider.getCurrentUser().getUserId());
     }
-
     //Todo: Add search capability in future
 
     @GetMapping("/{eventId}/shift-plans")
@@ -48,7 +55,6 @@ public class EventEndpoint {
         return eventService.getUserRelatedShiftPlansOfEvent(ConvertUtil.idToLong(eventId), userProvider.getCurrentUser().getUserId());
     }
 
-
     @GetMapping("/{eventId}/shift-plans-overview")
     //TODO Security
     @Operation(
@@ -59,34 +65,33 @@ public class EventEndpoint {
         return eventService.getEventShiftPlansOverview(ConvertUtil.idToLong(eventId), userProvider.getCurrentUser().getUserId());
     }
 
-    // TODO implement unavailability endpoints and service methods
-//    @GetMapping("/unavailability")
-//    // TODO Security
-//    @Operation(
-//        operationId = "getUnavailabilities",
-//        description = "Get unavailability periods of the current user"
-//    )
-//    public Collection<UnavailabilityDto> getUnavailabilities() {
-//        return null; // TODO: implement
-//    }
-//
-//    @PostMapping("/unavailability")
-//    // TODO Security
-//    @Operation(
-//        operationId = "createUnavailability",
-//        description = "Create a new unavailability period for the current user"
-//    )
-//    public UnavailabilityDto createUnavailability(@RequestBody UnavailabilityCreateDto createDto) {
-//        return null; // TODO: implement
-//    }
-//
-//    @DeleteMapping("/unavailability/{unavailabilityId}")
-//    // TODO Security
-//    @Operation(
-//        operationId = "deleteUnavailability",
-//        description = "Delete an existing unavailability period of the current user"
-//    )
-//    public void deleteUnavailability(@PathVariable String unavailabilityId) {
-//        // TODO: implement
-//    }
+    @GetMapping("/unavailability")
+    // TODO Security
+    @Operation(
+        operationId = "getUnavailabilities",
+        description = "Get unavailability periods of the current user"
+    )
+    public Collection<UnavailabilityDto> getUnavailabilities() {
+        return null; // TODO: implement
+    }
+
+    @PostMapping("/unavailability")
+    // TODO Security
+    @Operation(
+        operationId = "createUnavailability",
+        description = "Create a new unavailability period for the current user"
+    )
+    public UnavailabilityDto createUnavailability(@RequestBody UnavailabilityCreateDto createDto) {
+        return null; // TODO: implement
+    }
+
+    @DeleteMapping("/unavailability/{unavailabilityId}")
+    // TODO Security
+    @Operation(
+        operationId = "deleteUnavailability",
+        description = "Delete an existing unavailability period of the current user"
+    )
+    public void deleteUnavailability(@PathVariable String unavailabilityId) {
+        // TODO: implement
+    }
 }
