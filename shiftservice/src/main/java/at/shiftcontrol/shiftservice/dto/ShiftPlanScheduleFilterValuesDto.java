234e3155c1abde9f42e67c3834d96e2d780c955b
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-12-27 18:52:39 +0100

    #144 Progress on calendar lazy loading

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/ShiftPlanScheduleFilterValuesDto.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/ShiftPlanScheduleFilterValuesDto.java
@@ -3,13 +3,16 @@ package at.shiftcontrol.shiftservice.dto;
 import java.time.LocalDate;
 import java.util.Collection;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ShiftPlanScheduleFilterValuesDto {
+    @NotNull
     private Collection<LocationDto> locations;
+    @NotNull
     private Collection<RoleDto> roles;
 
     private LocalDate firstDate;
