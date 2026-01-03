a9d27b83c94b9c3cc42f53a686854272d65af093
Patrick Zdarsky <e12123697@student.tuwien.ac.at>
2026-01-03 15:20:39 +0100

    #173 Remove Builder annotation from ApplicationEvent class

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/event/ApplicationEvent.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/event/ApplicationEvent.java
@@ -1,11 +1,9 @@
 package at.shiftcontrol.shiftservice.event;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 @AllArgsConstructor
 public abstract class ApplicationEvent {
     public abstract String getRoutingKey();
