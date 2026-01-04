3b7cec3bd6d47e35e4b27232541dd9c7961dfc7c
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-04 20:49:47 +0100

    #202 Add activity crud in calendar

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/event/EventScheduleDto.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/event/EventScheduleDto.java
@@ -3,12 +3,16 @@ package at.shiftcontrol.shiftservice.dto.event;
 import java.util.Collection;
 
 import at.shiftcontrol.shiftservice.dto.activity.ActivityDto;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class EventScheduleDto {
+    @NotNull
     private EventDto event;
+    @NotNull
     private Collection<ActivityDto> activities;
 }
