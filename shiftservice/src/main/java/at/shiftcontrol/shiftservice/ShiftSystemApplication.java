9e8df7d0592f4fa6ce0bde2b64bbeb23cbf088c8
Patrick Zdarsky <e12123697@student.tuwien.ac.at>
2026-01-06 19:36:46 +0100

    #210 Add scheduling support and test event sender for activity events

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/ShiftSystemApplication.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/ShiftSystemApplication.java
@@ -2,8 +2,10 @@ package at.shiftcontrol.shiftservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShiftSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShiftSystemApplication.class, args);
