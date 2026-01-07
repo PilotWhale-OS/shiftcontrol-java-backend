f20c86967b6ffe75381da423db09695bd92f76ac
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-07 05:58:53 +0100

    #0 Fix event creation

--- shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/event/EventModificationDto.java
+++ shiftservice/src/main/java/at/shiftcontrol/shiftservice/dto/event/EventModificationDto.java
@@ -4,10 +4,14 @@ import java.time.Instant;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class EventModificationDto {
     @NotNull
